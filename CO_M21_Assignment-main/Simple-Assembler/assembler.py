c321015c798bbc40301b72afa6d8d4f7eb1ed80e
Gurmehak <84334413+gurmehakk@users.noreply.github.com>
2021-08-15 19:21:11 +0530

    Update assembler.py

--- CO_M21_Assignment-main/Simple-Assembler/assembler.py
+++ CO_M21_Assignment-main/Simple-Assembler/assembler.py
@@ -1,15 +1,22 @@
 import math
-
+def checkr():   #function to check if any variable is defined after a non var instruction is given
+    i=0
+    while(statements[i][0][0]=="var"):
+        i=i+1
+    for j in range(i,len(statements)):
+        if(statements[j][0][0]=="var"):
+            print("variable decleration after an instruction at line "+str(statements[j][1]))
+            exit(0)
 
 def error():
-    b = False
+    b = None
     for i in statements.keys():
         b = error1(statements[i])
-        if (b == False):
+        if (b == None):
             continue
         else:
             return b
-    return b
+    return False
 
 
 def error1(l):
@@ -18,22 +25,27 @@ def error1(l):
 
         return True
     elif ((l[0][0] == 'jmp' or l[0][0] == 'jlt' or l[0][0] == 'jgt' or l[0][0] == 'je') and (
-            l[0][1] in v.keys() or l[0][1] in reg.keys())):
+            l[0][1] in v.keys() or (l[0][1] in reg.keys() and l[0][1] !="FLAGS"))):
         print("Illegal memory address "+str(l[1]))
         return True
+    # to check errors in A type instructions
     elif (l[0][0] == "add" or l[0][0] == "sub" or l[0][0] == "mul" or l[0][0] == "xor" or l[0][0] == "or" or l[0][0] == "and"):
         if (len(l[0]) != 4):
             print("Wrong syntax used for instructions in line "+str(l[1]))
             return True
+        if(l[0][1]=="FLAGS"):
+            print("Illegal use of flags register at line "+str(l[1]))
+            return True
         elif (l[0][1] not in reg.keys() or l[0][2] not in reg.keys() or l[0][3] not in reg.keys()):
             print("Typos in register name in line "+str(l[1]))
             return True
+    # to check errors in both mov type instructions
     elif(l[0][0]=="mov"):
         if (len(l[0]) != 3):
-            print("Wrong syntax used for instructions in line"+str(l[1]))
+            print("Wrong syntax used for instructions in line "+str(l[1]))
             return True
-        elif (l[0][1] not in reg.keys()):
-            print("Typos in register name in line "+str(l[1]))
+        if (l[0][1] == "FLAGS"):
+            print("Illegal use of flags register at line "+str(l[1]))
             return True
         elif(l[0][2][0:1]=="R"):
             if(l[0][2] not in reg.keys()):
@@ -43,29 +55,33 @@ def error1(l):
             if (int(l[0][2][1:],10)<0 and int(l[0][2][1:],10)>255):
                 print("Invalid immidiete in line "+str(l[1]))
                 return True
-        else:
-            print("Wrong move instruction type in line "+str(l[1]))
-            return True
-    # b
-    elif ( l[0][0][0] == "rs" or l[0][0] == "ls"):
+    # to check errors in B type instructions
+    elif ( l[0][0] == "rs" or l[0][0] == "ls"):
         if (len(l[0]) != 3):
             print("Wrong syntax used for instructions in line "+str(l[1]))
+            return True
+        if (l[0][1] == "FLAGS" or l[0][2]=="FLAGS"):
+            print("Illegal use of flags register")
             return True
         elif (l[0][1] not in reg.keys()):
             print("Typos in register name "+str(l[1]))
             return True
         elif (l[0][2] not in reg.keys() and l[0][2] not in v.keys()):
             print("invalid register/variable name/immidiete in line "+str(l[1]))
+    # to check errors in C type instructions
     elif (l[0][0] == "div" or l[0][0] == "not" or l[0][0] == "cmp"+str(l[1])):
         if (len(l[0]) != 3):
             print("Wrong syntax used for instructions in line "+str(l[1]))
+            return True
+        if(l[0][0]=="not" and l[0][1]=="FLAGS"):
+            print("Illegal use of flags register")
             return True
         elif (l[0][2] not in reg.keys()):
             print("Typos in register name in line "+str(l[1]))
             return True
         elif (l[0][1] not in reg.keys()):
             print("Typo in register name in line "+str(l[1]))
-    # d
+    # to check errors in D type instructions
     elif (l[0][0] == "ld" or l[0][0] == "st"):
         if (len(l[0]) != 3):
             print("Wrong syntax used for instructions in line "+str(l[1]))
@@ -73,18 +89,21 @@ def error1(l):
         if (l[0][1] not in reg.keys()):
             print("Typo in register name in line "+str(l[1]))
             return True
+        if(l[0][0]=="ld" and l[0][1]=="FLAGS"):
+            print("Illegal use of flags register")
+            return True
         if l[0][2] not in v.keys():
             print("Typo in memory address in line "+str(l[1]))
             return True
-    # e
+    # to check errors in E type instructions
     elif (l[0][0] == "jmp" or l[0][0] == "jlt" or l[0][0] == "jgt" or l[0][0] == "je"):
-        if (len(l[0]) != 3):
+        if (len(l[0]) != 2):
             print("Wrong syntax used for instructions in line "+str(l[1]))
             return True
-        if l[0][2] not in v.keys():
+        if l[0][1] not in labels.keys():
             print("Typo in memory address in line "+str(l[1]))
             return True
-    # f
+    # to check errors in F type instructions
     elif (l[0][0] == "hlt"):
         if (len(l[0]) != 1):
             print("Wrong syntax used for instructions in line "+str(l[1]))
@@ -249,7 +268,7 @@ def jump_if_equal(l):
 def halt(l):
     return "1001100000000000"
 
-
+# the raise error line was used so that we can raise error during binary creation but then we handled the error generation using aa different fucntion
 def main(line):
     if (line[0][0] in op.keys()):
         if (line[0][0] == 'mov'):
@@ -386,7 +405,9 @@ reg = {'R0': ['000', 0], 'R1': ['001', 0], 'R2': ['010', 0], 'R3': ['011', 0], '
        'R5': ['101', 0], 'R6': ['110', 0], 'FLAGS': ['111', 0]}
 var = 0
 labels = {}
+reserved=["add","sub","mul","div","jmp","jgt","jlt","je","cpm","ld","st","not","xor","or","and","ls","rs","mov","hlt","R0","R1","R2","R3","R4","R5","R6","FLAGS","var",]
 line = ""
+vname="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_"
 # while (line!="hlt"):
 #         line = input()
 #         if (line.split(" ")[0] != "hlt" and (len(line.split(" ")) == 1)):
@@ -398,6 +419,7 @@ line = ""
 while (1):
     try:
         line = input()
+        line=line.strip()
         if (line != ""):
             if (line.split(" ")[0] != "hlt" and (len(line.split(" ")) == 1)):
                 print("Invalid Instruction at line "+str(var+1))
@@ -411,11 +433,25 @@ for i in statements.keys():
         if (len(statements[i][0]) == 1):
             print("Invalid Instruction at line "+str(statements[i][1]))
             exit(0)
+        if(statements[i][0][1] in reserved):
+            print("Reserved words cannot be used as variable names line no =>"+str(statements[i][1]))
+            exit(0)
+        for k in statements[i][0][1]:
+            if(k not in vname):
+                print("invalid literal in variable names at line "+str(statements[i][1]))
+                exit(0)
         v[statements[i][0][1]] = 0
     elif (statements[i][0][0][-1:] == ':'):
         if (statements[i][0][0][:-1] in labels):
             print("Two labels with same name -> Invalid Instruction at line "+str(statements[i][1]))
             exit(0)
+        if(statements[i][0][0][:-1] in reserved):
+            print("Reserved words cannot be used as label names line number =>"+str(statements[i][1]))
+            exit(0)
+        for k in statements[i][0][0][:-1]:
+            if(k not in vname):
+                print("invalid literal in label names at line "+str(statements[i][1]))
+                exit(0)
         # binary conversion
         labels[statements[i][0][0][:-1]] = convert(int(i) - len(v))
         del statements[i][0][0]
@@ -429,6 +465,7 @@ for i in statements.keys():
     if (statements[i][0][0] == "hlt" and statements[i][1] != len(statements) - 1):
         print("More than one hlt statement at line "+str(statements[i][1])+"\n")
         exit(0)
+checkr()
 if (error()):
     exit()
 else:
